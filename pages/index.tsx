3b3384ec7ec1415b48bda505ef93185286697734
Sean Swanson <seancswan@gmail.com>
2023-04-15 19:00:15 -0700

    Actually removed Shadcn, too much overhead.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { MainNav } from '@/components/main-nav';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const callAPI = async () => { 
+  const callAPI = async () => {
     try {
-      const res = await fetch('/api/gpt');
+      const res = await fetch("/api/gpt");
       const data = await res.json();
       console.log(data);
     } catch (error) {
       console.error(error);
-    } 
-  }
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-24">
-      <MainNav />
-      <h1 className="text-6xl font-bold">NextGPT</h1>
-      <button onClick={callAPI} className="border-white border rounded px-3 py-2 hover:bg-white hover:text-black">Call API</button>
+      <h1 className="text-6xl font-bold font-sans">NextGPT</h1>
+      <button
+        onClick={callAPI}
+        className="rounded border border-white px-3 py-2 hover:bg-white hover:text-black"
+      >
+        Call API
+      </button>
     </main>
-  )
+  );
 }
